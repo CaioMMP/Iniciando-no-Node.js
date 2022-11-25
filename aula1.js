6c9d209d8466595bc7419c30effddfb5eaf3b377
CaioM.M.dosP <mamedemacielpassos@gmail.com>
2022-11-25 10:50:23 -0300

    Adicionando Sugestão de placar Brasil e Suíça

--- aula1.js
+++ aula1.js
@@ -4,6 +4,7 @@ http.createServer(function(req, res) {
     res.end('Olá, Mundo! \n');
     res.writeHead(200, {'Content-Type':'text/html'});
     res.write('<h2>Brasil 2 x 0 Servia</h2> \n');
+    res.write('<h2>Brasil 3 x 1 Suíça</h2> \n');
     res.write('<p style = color: green;>' + Date().substring(16, 24) + '<br>');
     res.write(dia.diaDaSemana() + '</p><br>')
 }).listen(3000);
