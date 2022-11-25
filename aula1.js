5732f80645e5936dc68d48cf0fa0f7c18a255c2a
CaioM.M.dosP <mamedemacielpassos@gmail.com>
2022-11-25 10:44:21 -0300

    Adicionando biblioteca para retornar dia da semana

--- aula1.js
+++ aula1.js
@@ -1,7 +1,9 @@
 var http = require('http');
+var dia = require('./biblioteca');
 http.createServer(function(req, res) {
     res.end('Olá, Mundo! \n');
     res.writeHead(200, {'Content-Type':'text/html'});
     res.write('<h2>Brasil 2 x 0 Servia</h2> \n');
-    res.write(Date().substring(16, 24) + '<br>');
+    res.write('<p style = color: green;>' + Date().substring(16, 24) + '<br>');
+    res.write(dia.diaDaSemana() + '</p><br>')
 }).listen(3000);
