9bca15e527a8a530a0fb3f3567993a48ade6dbb5
CaioM.M.dosP <mamedemacielpassos@gmail.com>
2022-11-25 10:22:17 -0300

    Atualizações no arquivo

--- aula1.js
+++ aula1.js
@@ -1,5 +1,7 @@
 var http = require('http');
 http.createServer(function(req, res) {
     res.end('Olá, Mundo! \n');
-    res.write('Brasil 5 x 0 Servia');
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write('<h2>Brasil 2 x 0 Servia</h2> \n');
+    res.write(Date().substring(16, 24) + '<br>');
 }).listen(3000);
